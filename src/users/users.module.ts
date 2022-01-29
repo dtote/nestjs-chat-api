f666da16e243f39127b619627b520afa1a0091ed
Nestor Torres <alu0101021768@ull.edu.es>
2022-01-29 06:38:31 +0000

    feat: adds jwt strategy and module to the users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
-
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { JwtStrategy } from './dtos/jwt.strategy'
+import { UsersController } from './users.controller'
+import { UsersRepository } from './users.repository'
+import { UsersService } from './users.service'
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({ secret: 'mySecret', signOptions: { expiresIn: 3600 } }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class UsersModule {}
