af84d45c8077237c6b1183b4656a02119cc499c4
Nestor Torres <alu0101021768@ull.edu.es>
2022-01-29 06:36:37 +0000

    feat: adds required packages for authentication with jwt (passport, jwt), jwt payload interface and strategy. Also adds pg package for handling db connection with postgresql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common'
-import { AuthController } from './auth/auth.controller'
-import { AuthModule } from './auth/auth.module'
-import { AuthService } from './auth/auth.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { MessagesModule } from './messages/messages.module'
 import { NotificationsModule } from './notifications/notifications.module'
 import { UsersModule } from './users/users.module'
-
 @Module({
-  imports: [UsersModule, AuthModule, NotificationsModule, MessagesModule],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    NotificationsModule,
+    MessagesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'pass',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/users/dtos/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { InjectRepository } from '@nestjs/typeorm'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { User } from '../user.entity'
+import { UsersRepository } from '../users.repository'
+import { JwtPayload } from './jwt-payload.interface'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(@InjectRepository(UsersRepository) private usersRepository: UsersRepository) {
+    super({
+      secretOrKey: 'mySecret',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    })
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload
+    const user = await this.usersRepository.findOne({ email })
+
+    if (!user) throw new UnauthorizedException()
+
+    return user
+  }
+}
